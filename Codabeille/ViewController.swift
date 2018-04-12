7a3ac07e507578d8d902bb24c2b7ff8949c96084
Christian Stenger <stenger@MacBook-Pro-de-Christian.local>
2018-04-12 15:43:47 +0200

    Premier commit

--- Codabeille/ViewController.swift
+++ Codabeille/ViewController.swift
@@ -10,15 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var abeilleVue: UIImageView!
+    
+    @IBOutlet weak var bouton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        abeilleVue.animationImages = [#imageLiteral(resourceName: "abeille1"),#imageLiteral(resourceName: "abeille2"),#imageLiteral(resourceName: "abeille3"),#imageLiteral(resourceName: "abeille4"),#imageLiteral(resourceName: "abeille5"),#imageLiteral(resourceName: "abeille6")]
+        abeilleVue.animationDuration = 0.5
+        abeilleVue.animationRepeatCount = 0
+        abeilleVue.startAnimating()
+        
+                
+        }
+    
+    @IBAction func boutonAction(_ sender: Any) {
+        if abeilleVue.isAnimating {
+            abeilleVue.stopAnimating()
+            bouton.setTitle("ANIMER", for: .normal)
+        } else {
+            abeilleVue.startAnimating()
+        bouton.setTitle("STOP", for: .normal)
+        }
+    
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
